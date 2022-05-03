cab67bc47f2de776b7a4ea0c90e17acabe3f87d4
Obekas <ogaten27@gmail.com>
2022-05-02 17:25:15 -0700

    initial commit

--- served-files/register.js
+++ served-files/register.js
@@ -8,3 +8,26 @@ const clickBtnEffect = (e)=>{
 function register(){
 
 }
+const useDatePicker = ()=>{
+    //Target Element
+    const datePicker = document.querySelector('.date-picker');
+
+    //If Element Doesn't Exist
+    if(!datePicker)return;
+
+    //Sections Declaration
+    const displaySection = document.createElement('div');
+    const yearSection = document.createElement('div');
+    const monthsSection = document.createElement('div');
+    const daysSection = document.createElement('div');
+
+    datePicker.appendChild(displaySection);
+
+    //Variables Declaration
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+    let day = new Date().getDate();
+    
+    displaySection.textContent = `${year}-${month+1}-${day}`;
+}
+useDatePicker();
